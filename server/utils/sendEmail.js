97ee166caeee475a6fc14b4740e6ceb2795dab33
Kunal Yadav <kunalyadav.ky1507@gmail.com>
2026-07-06 12:33:42 +0530

    Commit changes

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -2,7 +2,12 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
 
-  service: "gmail",
+  host: "smtp.gmail.com",
+
+  port: 587,
+
+  secure: false,
+
 
   auth: {
 

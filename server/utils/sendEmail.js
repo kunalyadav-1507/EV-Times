1f5e03a77a73ecece46fbd5b7b778953ea525b5b
Kunal Yadav <kunalyadav.ky1507@gmail.com>
2026-07-06 15:59:14 +0530

    Commit changes

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,14 +1,21 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
+
+  host: "smtp-relay.brevo.com",
+
   port: 587,
+
   secure: false,
-  family: 4,
+
   auth: {
+
     user: process.env.EMAIL_USER,
+
     pass: process.env.EMAIL_PASS
+
   }
+
 });
 
 const sendEmail = async (to, subject, text) => {
